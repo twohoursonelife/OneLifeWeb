5a8f9b19f3935ba0471a71dfd4ae12a04ea7a43b
Connor Smith <connorhsmith@gmail.com>
2026-06-04 23:38:19 +1000

    Clean up.

--- web/public/modTools/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// edit settings.php to change server' settings
-include( "settings.php" );
- 
-?>
--- web/public/modTools/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Basic settings
-// You must set these for the server to work
-
-require '../config.php';
-
-?>
